f82a503ae7a5a8c7fabd032373a25b3d32b244aa
Md Shamoel Farooqui <farooquishamoel@gmail.com>
2026-02-06 00:34:02 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { io } from 'socket.io-client'
 
-import Landing from './pages/Landing'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
@@ -29,7 +28,6 @@ import useUpdateLocation from './hooks/useUpdateLocation'
 
 import { setSocket } from './redux/userSlice'
 
-// Backend server URL
 export const serverUrl = "https://hostel-hungry.onrender.com"
 
 function App() {
@@ -44,7 +42,6 @@ function App() {
   useGetItemsByCity()
   useGetMyOrders()
 
-  /*  SOCKET */
   useEffect(() => {
     if (!userData?._id) return
 
@@ -64,14 +61,17 @@ function App() {
     return () => {
       socketInstance.disconnect()
     }
-  }, [userData])
+  }, [userData?._id]) // Optimized dependency
 
   return (
     <Routes>
+      {/* ROOT REDIRECT: Landing Page Removed */}
+      <Route 
+        path='/' 
+        element={userData ? <Navigate to="/home" /> : <Navigate to="/signin" />} 
+      />
 
-      {/* PUBLIC ROUTES */}
-      <Route path='/' element={<Landing />} />
-
+      {/* AUTH ROUTES */}
       <Route
         path='/signin'
         element={!userData ? <SignIn /> : <Navigate to="/home" />}
@@ -84,11 +84,9 @@ function App() {
         path='/forgot-password'
         element={!userData ? <ForgotPassword /> : <Navigate to="/home" />}
       />
-
-      {/* EMAIL VERIFICATION */}
       <Route path='/verify-email' element={<VerifyEmail />} />
 
-      {/*  PROTECTED ROUTES  */}
+      {/* PROTECTED ROUTES */}
       <Route
         path='/home'
         element={userData ? <Home /> : <Navigate to="/signin" />}
@@ -129,100 +127,11 @@ function App() {
         path='/shop/:shopId'
         element={userData ? <Shop /> : <Navigate to="/signin" />}
       />
-
     </Routes>
   )
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-/*
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import SignUp from './pages/SignUp'
-import SignIn from './pages/SignIn'
-import ForgotPassword from './pages/ForgotPassword'
-import useGetCurrentUser from './hooks/useGetCurrentUser'
-import { useDispatch, useSelector } from 'react-redux'
-import Home from './pages/Home'
-import useGetCity from './hooks/useGetCity'
-import useGetMyshop from './hooks/useGetMyShop'
-import CreateEditShop from './pages/CreateEditShop'
-import AddItem from './pages/AddItem'
-import EditItem from './pages/EditItem'
-import useGetShopByCity from './hooks/useGetShopByCity'
-import useGetItemsByCity from './hooks/useGetItemsByCity'
-import CartPage from './pages/CartPage'
-import CheckOut from './pages/CheckOut'
-import OrderPlaced from './pages/OrderPlaced'
-import MyOrders from './pages/MyOrders'
-import useGetMyOrders from './hooks/useGetMyOrders'
-import useUpdateLocation from './hooks/useUpdateLocation'
-import TrackOrderPage from './pages/TrackOrderPage'
-import Shop from './pages/Shop'
-import { useEffect } from 'react'
-import { io } from 'socket.io-client'
-import { setSocket } from './redux/userSlice'
-
-export const serverUrl="https://hostel-hungry.onrender.com"
-function App() {
-    const {userData}=useSelector(state=>state.user)
-    const dispatch=useDispatch()
-  useGetCurrentUser()
-useUpdateLocation()
-  useGetCity()
-  useGetMyshop()
-  useGetShopByCity()
-  useGetItemsByCity()
-  useGetMyOrders()
-
-  useEffect(()=>{
-const socketInstance=io(serverUrl,{withCredentials:true})
-dispatch(setSocket(socketInstance))
-socketInstance.on('connect',()=>{
-if(userData){
-  socketInstance.emit('identity',{userId:userData._id})
-}
-})
-return ()=>{
-  socketInstance.disconnect()
-}
-  },[userData?._id])
-
-  return (
-   <Routes>
-    <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
-    <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
-      <Route path='/forgot-password' element={!userData?<ForgotPassword/>:<Navigate to={"/"}/>}/>
-      <Route path='/' element={userData?<Home/>:<Navigate to={"/signin"}/>}/>
-<Route path='/create-edit-shop' element={userData?<CreateEditShop/>:<Navigate to={"/signin"}/>}/>
-<Route path='/add-item' element={userData?<AddItem/>:<Navigate to={"/signin"}/>}/>
-<Route path='/edit-item/:itemId' element={userData?<EditItem/>:<Navigate to={"/signin"}/>}/>
-<Route path='/cart' element={userData?<CartPage/>:<Navigate to={"/signin"}/>}/>
-<Route path='/checkout' element={userData?<CheckOut/>:<Navigate to={"/signin"}/>}/>
-<Route path='/order-placed' element={userData?<OrderPlaced/>:<Navigate to={"/signin"}/>}/>
-<Route path='/my-orders' element={userData?<MyOrders/>:<Navigate to={"/signin"}/>}/>
-<Route path='/track-order/:orderId' element={userData?<TrackOrderPage/>:<Navigate to={"/signin"}/>}/>
-<Route path='/shop/:shopId' element={userData?<Shop/>:<Navigate to={"/signin"}/>}/>
-   </Routes>
-  )
-}
-
-export default App
-*/
-
-
-
-
-
 
 // import React, { useEffect } from 'react'
 // import { Navigate, Route, Routes } from 'react-router-dom'
@@ -243,6 +152,7 @@ export default App
 // import MyOrders from './pages/MyOrders'
 // import TrackOrderPage from './pages/TrackOrderPage'
 // import Shop from './pages/Shop'
+// import VerifyEmail from './pages/VerifyEmail' 
 
 // import useGetCurrentUser from './hooks/useGetCurrentUser'
 // import useGetCity from './hooks/useGetCity'
@@ -254,6 +164,7 @@ export default App
 
 // import { setSocket } from './redux/userSlice'
 
+// // Backend server URL
 // export const serverUrl = "https://hostel-hungry.onrender.com"
 
 // function App() {
@@ -268,29 +179,33 @@ export default App
 //   useGetItemsByCity()
 //   useGetMyOrders()
 
+//   /*  SOCKET */
 //   useEffect(() => {
-//     const socketInstance = io(serverUrl, { withCredentials: true })
+//     if (!userData?._id) return
+
+//     const socketInstance = io(serverUrl, {
+//       withCredentials: true,
+//       transports: ["websocket"],
+//     })
+
 //     dispatch(setSocket(socketInstance))
 
-//     socketInstance.on('connect', () => {
-//       if (userData) {
-//         socketInstance.emit('identity', { userId: userData._id })
-//       }
+//     socketInstance.on("connect", () => {
+//       socketInstance.emit("identity", {
+//         userId: userData._id,
+//       })
 //     })
 
 //     return () => {
 //       socketInstance.disconnect()
 //     }
-//   }, [userData?._id])
+//   }, [userData])
 
 //   return (
 //     <Routes>
 
-//      <Route
-//   path="/"
-//   element={userData ? <Navigate to="/home" /> : <Landing />}
-// />
-
+//       {/* PUBLIC ROUTES */}
+//       <Route path='/' element={<Landing />} />
 
 //       <Route
 //         path='/signin'
@@ -305,7 +220,10 @@ export default App
 //         element={!userData ? <ForgotPassword /> : <Navigate to="/home" />}
 //       />
 
-//       {/* PROTECTED ROUTES */}
+//       {/* EMAIL VERIFICATION */}
+//       <Route path='/verify-email' element={<VerifyEmail />} />
+
+//       {/*  PROTECTED ROUTES  */}
 //       <Route
 //         path='/home'
 //         element={userData ? <Home /> : <Navigate to="/signin" />}
@@ -352,3 +270,220 @@ export default App
 // }
 
 // export default App
+
+
+
+
+
+
+
+
+
+// /*
+// import React from 'react'
+// import { Navigate, Route, Routes } from 'react-router-dom'
+// import SignUp from './pages/SignUp'
+// import SignIn from './pages/SignIn'
+// import ForgotPassword from './pages/ForgotPassword'
+// import useGetCurrentUser from './hooks/useGetCurrentUser'
+// import { useDispatch, useSelector } from 'react-redux'
+// import Home from './pages/Home'
+// import useGetCity from './hooks/useGetCity'
+// import useGetMyshop from './hooks/useGetMyShop'
+// import CreateEditShop from './pages/CreateEditShop'
+// import AddItem from './pages/AddItem'
+// import EditItem from './pages/EditItem'
+// import useGetShopByCity from './hooks/useGetShopByCity'
+// import useGetItemsByCity from './hooks/useGetItemsByCity'
+// import CartPage from './pages/CartPage'
+// import CheckOut from './pages/CheckOut'
+// import OrderPlaced from './pages/OrderPlaced'
+// import MyOrders from './pages/MyOrders'
+// import useGetMyOrders from './hooks/useGetMyOrders'
+// import useUpdateLocation from './hooks/useUpdateLocation'
+// import TrackOrderPage from './pages/TrackOrderPage'
+// import Shop from './pages/Shop'
+// import { useEffect } from 'react'
+// import { io } from 'socket.io-client'
+// import { setSocket } from './redux/userSlice'
+
+// export const serverUrl="https://hostel-hungry.onrender.com"
+// function App() {
+//     const {userData}=useSelector(state=>state.user)
+//     const dispatch=useDispatch()
+//   useGetCurrentUser()
+// useUpdateLocation()
+//   useGetCity()
+//   useGetMyshop()
+//   useGetShopByCity()
+//   useGetItemsByCity()
+//   useGetMyOrders()
+
+//   useEffect(()=>{
+// const socketInstance=io(serverUrl,{withCredentials:true})
+// dispatch(setSocket(socketInstance))
+// socketInstance.on('connect',()=>{
+// if(userData){
+//   socketInstance.emit('identity',{userId:userData._id})
+// }
+// })
+// return ()=>{
+//   socketInstance.disconnect()
+// }
+//   },[userData?._id])
+
+//   return (
+//    <Routes>
+//     <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
+//     <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
+//       <Route path='/forgot-password' element={!userData?<ForgotPassword/>:<Navigate to={"/"}/>}/>
+//       <Route path='/' element={userData?<Home/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/create-edit-shop' element={userData?<CreateEditShop/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/add-item' element={userData?<AddItem/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/edit-item/:itemId' element={userData?<EditItem/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/cart' element={userData?<CartPage/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/checkout' element={userData?<CheckOut/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/order-placed' element={userData?<OrderPlaced/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/my-orders' element={userData?<MyOrders/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/track-order/:orderId' element={userData?<TrackOrderPage/>:<Navigate to={"/signin"}/>}/>
+// <Route path='/shop/:shopId' element={userData?<Shop/>:<Navigate to={"/signin"}/>}/>
+//    </Routes>
+//   )
+// }
+
+// export default App
+// */
+
+
+
+
+
+
+// // import React, { useEffect } from 'react'
+// // import { Navigate, Route, Routes } from 'react-router-dom'
+// // import { useDispatch, useSelector } from 'react-redux'
+// // import { io } from 'socket.io-client'
+
+// // import Landing from './pages/Landing'
+// // import SignUp from './pages/SignUp'
+// // import SignIn from './pages/SignIn'
+// // import ForgotPassword from './pages/ForgotPassword'
+// // import Home from './pages/Home'
+// // import CreateEditShop from './pages/CreateEditShop'
+// // import AddItem from './pages/AddItem'
+// // import EditItem from './pages/EditItem'
+// // import CartPage from './pages/CartPage'
+// // import CheckOut from './pages/CheckOut'
+// // import OrderPlaced from './pages/OrderPlaced'
+// // import MyOrders from './pages/MyOrders'
+// // import TrackOrderPage from './pages/TrackOrderPage'
+// // import Shop from './pages/Shop'
+
+// // import useGetCurrentUser from './hooks/useGetCurrentUser'
+// // import useGetCity from './hooks/useGetCity'
+// // import useGetMyshop from './hooks/useGetMyShop'
+// // import useGetShopByCity from './hooks/useGetShopByCity'
+// // import useGetItemsByCity from './hooks/useGetItemsByCity'
+// // import useGetMyOrders from './hooks/useGetMyOrders'
+// // import useUpdateLocation from './hooks/useUpdateLocation'
+
+// // import { setSocket } from './redux/userSlice'
+
+// // export const serverUrl = "https://hostel-hungry.onrender.com"
+
+// // function App() {
+// //   const { userData } = useSelector(state => state.user)
+// //   const dispatch = useDispatch()
+
+// //   useGetCurrentUser()
+// //   useUpdateLocation()
+// //   useGetCity()
+// //   useGetMyshop()
+// //   useGetShopByCity()
+// //   useGetItemsByCity()
+// //   useGetMyOrders()
+
+// //   useEffect(() => {
+// //     const socketInstance = io(serverUrl, { withCredentials: true })
+// //     dispatch(setSocket(socketInstance))
+
+// //     socketInstance.on('connect', () => {
+// //       if (userData) {
+// //         socketInstance.emit('identity', { userId: userData._id })
+// //       }
+// //     })
+
+// //     return () => {
+// //       socketInstance.disconnect()
+// //     }
+// //   }, [userData?._id])
+
+// //   return (
+// //     <Routes>
+
+// //      <Route
+// //   path="/"
+// //   element={userData ? <Navigate to="/home" /> : <Landing />}
+// // />
+
+
+// //       <Route
+// //         path='/signin'
+// //         element={!userData ? <SignIn /> : <Navigate to="/home" />}
+// //       />
+// //       <Route
+// //         path='/signup'
+// //         element={!userData ? <SignUp /> : <Navigate to="/home" />}
+// //       />
+// //       <Route
+// //         path='/forgot-password'
+// //         element={!userData ? <ForgotPassword /> : <Navigate to="/home" />}
+// //       />
+
+// //       {/* PROTECTED ROUTES */}
+// //       <Route
+// //         path='/home'
+// //         element={userData ? <Home /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/create-edit-shop'
+// //         element={userData ? <CreateEditShop /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/add-item'
+// //         element={userData ? <AddItem /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/edit-item/:itemId'
+// //         element={userData ? <EditItem /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/cart'
+// //         element={userData ? <CartPage /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/checkout'
+// //         element={userData ? <CheckOut /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/order-placed'
+// //         element={userData ? <OrderPlaced /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/my-orders'
+// //         element={userData ? <MyOrders /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/track-order/:orderId'
+// //         element={userData ? <TrackOrderPage /> : <Navigate to="/signin" />}
+// //       />
+// //       <Route
+// //         path='/shop/:shopId'
+// //         element={userData ? <Shop /> : <Navigate to="/signin" />}
+// //       />
+
+// //     </Routes>
+// //   )
+// // }
+
+// // export default App
